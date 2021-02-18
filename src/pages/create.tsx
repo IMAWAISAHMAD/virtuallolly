557fa3a774e5d9f3331811f21c313594aa60a154
Awais Ahmad <awais.ahmad1990@yahoo.com>
2021-02-18 12:59:01 +0500

    Initial create lolly page completed

---
+++ src/pages/create.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import LollyForm from '../components/LollyForm'
+export default function create() {
+    return (
+        <div>
+            <LollyForm/>
+        </div>
+    )
+}
