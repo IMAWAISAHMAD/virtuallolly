b3b832a01a713c8bfedab8318a861dd822ddf99b
Awais Ahmad <awais.ahmad1990@yahoo.com>
2021-02-24 17:29:40 +0500

    Unused files deleted

--- src/pages/create.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import LollyForm from '../components/LollyForm'
-export default function create() {
-    return (
-        <div>
-            <LollyForm/>
-        </div>
-    )
-}
