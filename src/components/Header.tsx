e8642491a68c27c41674d1d279258bd2dfaf4ff2
Awais Ahmad <awais.ahmad1990@yahoo.com>
2021-02-25 12:01:15 +0500

    Header component update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import {Text} from 'theme-ui'
 
 export default function Header() {
     return (
         <div style={{textAlign:"center"}}>
+          <Text
+          sx={{
+            fontSize: 25,
+            fontFamily: "Yellowtail",
+            color: "black",
+            textShadow: "#e0f 0 0 8px",
+            textAlign: "center",
+          }}
+          >
           <h1>Virtual Lolly</h1> 
-          <p>Because we all know someone who deserves some sugar.</p> 
+          <p>Because we all know someone who deserves some sugar.</p>
+          </Text>
         </div>
     )
 }
